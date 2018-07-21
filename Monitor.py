7d3eb7c3ad3334a13d731eeddc0da54184cd7157
Zhang_JK <zjkisgood@126.com>
2018-07-21 19:07:30 +0800

    MonitorSession return a frame

--- Monitor.py
+++ Monitor.py
@@ -1,10 +1,15 @@
 
+import cv2
+from Tools import NotCompleted, CamConnectError
+
 class Monitor:
     def __init__(self, id, addr, physical_location, type):
         self.id = id
         self.addr = addr
         self.physical_location = physical_location
         self.type = type
+        self.connect_addr = None
+        self.cap = None
 
     def get_id(self):
         return self.id
@@ -20,3 +25,49 @@ class Monitor:
 
     def __str__(self):
         return "Monitor({}, {}, {}, {})".format(self.id, self.addr, self.physical_location, self.type)
+
+    def setAddr(self, addr):
+        """
+        :param addr: for instance:  rtsp://192.168.1.60/stream2
+        :return:
+        """
+        self.connect_addr = addr
+
+    def connect(self):
+        if self.connect_addr.startwith("rtmp://"):
+            self.cap = cv2.VideoCapture()
+            self.cap.open(self.connect_addr)
+        else:
+            self.cap = cv2.VideoCapture(self.connect_addr)
+
+        if not self.cap.isOpened():
+            raise CamConnectError()
+
+    def section(self):
+        """
+            This method shall be used to fetch a pic from a current monitor stream
+        :return:
+        """
+
+        if self.cap.isOpened():
+            flag, frame = self.cap.read()
+
+            return frame
+        else:
+            raise CamConnectError()
+
+    def clean(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    session1 = Monitor()
+    session1.setAddr("rtmp://192.168.137.149/live")
+    session1.connect()
+    frame = session1.section()
+    cv2.namedWindow("Image")
+    cv2.imshow("Image", frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    session1.clean()
