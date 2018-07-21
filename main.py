b7e6c5fe21dc5578dd1abab2061631f1f68c4415
Rocon <qiujiangkun@foxmail.com>
2018-07-21 14:04:41 +0800

    new files

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from MonitorPool import  MonitorPool
 import MonitorSession
+
+
+class Backend:
+    def __init__(self):
+        self.monitor_pool = MonitorPool()
+
+    def read_config(self):
+        pass
